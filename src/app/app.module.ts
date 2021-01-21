d53bd1702375a74da184e67dc7b91814b610d2fd
Vir1oN <xsv7yy@gmail.com>
2021-01-21 17:29:24 +0200

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CitiesComponent } from './cities/cities.component';
-import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CitiesComponent,
-    CityComponent
   ],
   imports: [
     BrowserModule
