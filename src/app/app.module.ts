7ddaad0d7d9f2630cec32b561e5b21dc5f1f8af9
Vir1oN <xsv7yy@gmail.com>
2021-01-28 20:40:58 +0200

    dz-28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+import {UserResolveService} from './services/resolve/user-resolve.service';
+import { UserComponent } from './components/user/user.component';
+import { FullUserComponent } from './components/full-user/full-user.component';
+
+const routes: Routes = [
+  {path: 'users', component: UsersComponent, resolve: {usersArr: UserResolveService},
+   children: [
+     {path: ':id', component: FullUserComponent}
+   ]},
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'posts', }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    UserComponent,
+    FullUserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/post/post.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+
+
+@NgModule({
+  declarations: [PostsComponent, PostComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostModule { }
